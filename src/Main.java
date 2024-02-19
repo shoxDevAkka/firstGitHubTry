d7216fcd442871cee1ec0883073722ba5cbca3ff
shoxDev <shox.intelj.s17@gmail.com>
2024-02-19 13:29:39 +0500

    my first commit of java file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Hi Java I am pleased to meet you");
+       System.out.println(args[0] + " - " + args[1] + " " + args[2]);
+       System.out.println("I am learning a lot of hints or ways to learn you");
+    }
+}
